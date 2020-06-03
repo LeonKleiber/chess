0e0ffb49d56f0f58bffde78e84b355e42ff32fdc
Doerksen <65653807+Doerksen@users.noreply.github.com>
2020-06-03 10:57:57 +0200

    Rook Movement

--- src/data/Rook.java
+++ src/data/Rook.java
@@ -11,7 +11,23 @@ public class Rook extends ChessPiece {
 
     @Override
     public Position[] getMovementOptions(Position position) {
-        position.x = 4;
-        return new Position[0];
+        Position[] positions = new Position[14];
+        for (int i = 1; i < 8; i++) {
+            if(position.x + i < 9) {
+                positions[i - 1] = new Position(position.x + i, position.y);
+            }else if(position.x - i >= 0){
+                positions[i - 1] = new Position(position.x - i, position.y);
+            }
+            System.out.println(positions[i - 1].x + " und " + positions[i - 1].y);
+        }
+        for (int i = 1; i < 8; i++){
+            if(position.y + i < 9) {
+                positions[i - 1] = new Position(position.x, position.y + i);
+            }else if(position.y - i >= 0){
+                positions[i - 1] = new Position(position.x, position.y - 1);
+            }
+            System.out.println(positions[i - 1].x + " und " + positions[i - 1].y);
+        }
+        return positions;
     }
 }
