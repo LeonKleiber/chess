6a1ede19d5b5d2df05cf8736ef36e8ba8dd2a0ae
Jaehaerys Themon <54642148+JaehaerysThemon@users.noreply.github.com>
2020-06-03 09:50:26 +0200

    Update ChessField.java

--- src/gui/ChessField.java
+++ src/gui/ChessField.java
@@ -7,7 +7,7 @@ import java.awt.*;
 
 public class ChessField extends JButton {
 
-    Position position;
+    Position position; 
 
     ChessField (int x, int y , Color color){
         setBackground(color);
