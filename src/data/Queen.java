1ada3d450fe08811da4ceeaa3753651847d0fb68
Doerksen <65653807+Doerksen@users.noreply.github.com>
2020-06-17 11:13:55 +0200

    Queen Finished

--- src/data/Queen.java
+++ src/data/Queen.java
@@ -22,6 +22,63 @@ public class Queen extends ChessPiece {
         ArrayList<Position> b = new ArrayList<Position>();
         ArrayList<Position> l = new ArrayList<Position>();
         ArrayList<Position> r = new ArrayList<Position>();
+
+        int y = position.y;
+        for (int i = position.x; i < 7; i++) {
+            if(y < 7 && y >= 0) {
+                fl.add(new Position(i + 1, y + 1));
+                y++;
+            }
+        }
+
+        y = position.y;
+        for(int i = position.x; i > 0; i--){
+            if(y < 7 && y >= 0) {
+                fr.add(new Position(i - 1, y + 1));
+                y++;
+            }
+        }
+
+        y = position.y;
+        for (int i = position.x; i < 7; i++) {
+            if(y <= 7 && y > 0) {
+                bl.add(new Position(i + 1, y - 1));
+                y--;
+            }
+        }
+
+        y = position.y;
+        for(int i = position.x; i > 0; i--){
+            if(y <= 7 && y > 0) {
+                br.add(new Position(i - 1, y - 1));
+                y--;
+            }
+        }
+
+        for (int i = position.x; i < 7; i++) {
+            fl.add(new Position(i + 1, position.y));
+        }
+
+        for(int i = position.x; i > 0; i--){
+            fr.add(new Position(i-1, position.y));
+        }
+
+        for (int i = position.y; i < 7; i++) {
+            bl.add(new Position(position.x, i + 1));
+        }
+
+        for(int i = position.y; i > 0; i--){
+            br.add(new Position(position.x, i - 1));
+        }
+
+        positions.add(fl);
+        positions.add(fr);
+        positions.add(bl);
+        positions.add(br);
+        positions.add(f);
+        positions.add(b);
+        positions.add(l);
+        positions.add(r);
         return positions;
     }
 }
