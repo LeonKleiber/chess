2ef4995f9293f9f48a2e5842e2aed3d9f9613ff1
Doerksen <65653807+Doerksen@users.noreply.github.com>
2020-06-17 08:30:49 +0200

    Bishop

--- src/data/Bishop.java
+++ src/data/Bishop.java
@@ -18,31 +18,37 @@ public class Bishop extends ChessPiece {
         ArrayList<Position> fr = new ArrayList<Position>();
         ArrayList<Position> bl = new ArrayList<Position>();
         ArrayList<Position> br = new ArrayList<Position>();
-         /*   for (int i = position.x; i < 7; i++) {
-                fl.add(new Position(i + 1, position.y));
-                System.out.println(( i + 1) + " und " + position.y);
+        int y = position.y;
+            for (int i = position.x; i < 7; i++) {
+                fl.add(new Position(i + 1, y + 1));
+                System.out.println((i + 1) + " und " + (y + 1));
+                y++;
             }
-
+            y = position.y;
             for(int i = position.x; i > 0; i--){
-                fr.add(new Position(i-1, position.y));
-                System.out.println((i-1) +" und "+position.y);
+                fr.add(new Position(i - 1, y - 1));
+                System.out.println((i - 1) +" und "+ (y + 1));
+                y++;
             }
 
+                y = position.x;
             for (int i = position.y; i < 7; i++) {
-                bl.add(new Position(position.x, i + 1));
-                System.out.println(position.x + " und " + (i + 1));
+                bl.add(new Position(y + 1, i + 1));
+                System.out.println((y + 1) + " und " + (i + 1));
+                y++;
             }
 
+                y = position.x;
             for(int i = position.y; i > 0; i--){
-                br.add(new Position(position.x, i - 1));
+                br.add(new Position((y -1), i - 1));
                 System.out.println(position.x + " und " + (i - 1));
+                y++;
             }
-            positions.add(fw);
-            positions.add(bw);
-            positions.add(le);
-            positions.add(ri);
-        }*/
+            positions.add(fl);
+            positions.add(fr);
+            positions.add(bl);
+            positions.add(br);
         return positions;
-    }
+        }
 
-}
+    }
